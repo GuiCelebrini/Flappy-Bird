46365e2c0457129c771967ca492348e742f7571f
GuiCelebrini <celebrinig@gmail.com>
2021-03-17 17:25:21 -0300

    Adicionando pontuação e permitindo parada antes do início

--- core/src/com/cursoandroid/flappybird/FlappyBird.java
+++ core/src/com/cursoandroid/flappybird/FlappyBird.java
@@ -2,8 +2,10 @@ package com.cursoandroid.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.Random;
@@ -14,13 +16,18 @@ public class FlappyBird extends ApplicationAdapter {
 	private Random numAleatorio;
 	private Texture passaro[], fundo;
 	private Texture canoTopo, canoBaixo;
+	private BitmapFont fonte;
 
 	//variáveis de configuração
-	private float variacao = 0;
-	private float velocidadeQueda = 0;
+	private boolean jogoComecou = false; //caso tenha começado vai passar para true
+	private boolean marcouPonto = false; //caso o cano passe do pássaro se tornará true
 
+	private int pontuacao = 0;
 	private int larguraTela;
 	private int alturaTela;
+
+	private float variacao = 0;
+	private float velocidadeQueda = 0;
 	private float posicaoInicialVertical;
 	private float posicaoHorizontalCano, posicaoVerticalCanoTopo, posicaoVerticalCanoBaixo;
 	private float espacoEntreCanos;
@@ -33,6 +40,10 @@ public class FlappyBird extends ApplicationAdapter {
 
 		batch = new SpriteBatch();
 		numAleatorio = new Random();
+		fonte = new BitmapFont();
+		fonte.setColor(Color.WHITE);
+		fonte.getData().setScale(6);
+
 		passaro = new Texture[3];
 		passaro[0] = new Texture("passaro1.png");
 		passaro[1] = new Texture("passaro2.png");
@@ -44,7 +55,7 @@ public class FlappyBird extends ApplicationAdapter {
 		larguraTela = Gdx.graphics.getWidth();
 		alturaTela = Gdx.graphics.getHeight();
 		posicaoInicialVertical = alturaTela/2;
-		posicaoHorizontalCano = larguraTela - 100;
+		posicaoHorizontalCano = larguraTela;
 		posicaoVerticalCanoTopo = (alturaTela / 2);
 		posicaoVerticalCanoBaixo = -120;
 		espacoEntreCanos = 500;
@@ -55,27 +66,44 @@ public class FlappyBird extends ApplicationAdapter {
 
 		tempoRenderizacao = Gdx.graphics.getDeltaTime();
 
-		//fazendo os canos se moverem, reaparecerem e variando a posição deles a cada nova aparição
-		posicaoHorizontalCano -= tempoRenderizacao * 300;
-		if (posicaoHorizontalCano < -100){
-			posicaoHorizontalCano = larguraTela;
-			variacaoCanos = numAleatorio.nextInt(500)-250;
-		}
+		//selecionando o que vai ocorrer e o que não vai enquando o jogo não começa
+		if (!jogoComecou){
 
-        //adicionando "gravidade" e queda ao pássaro
-		if (posicaoInicialVertical > 20 || velocidadeQueda < 0) {
-			velocidadeQueda += 0.5;
-			posicaoInicialVertical = posicaoInicialVertical - velocidadeQueda;
-		}
-
-		//adicionando evento de clique e evitando que o pássaro suba mais que o limite da tela
-		if (posicaoInicialVertical < alturaTela) {
 			if (Gdx.input.justTouched()) {
-				velocidadeQueda -= 12;
+				jogoComecou = true;
 			}
+
 		} else {
-			posicaoInicialVertical = alturaTela - 50;
-		}
+
+            //fazendo os canos se moverem, reaparecerem, variando a posição deles a cada nova aparição e configurando o marcouPonto
+            posicaoHorizontalCano -= tempoRenderizacao * 300;
+            if (posicaoHorizontalCano < -100) {
+                posicaoHorizontalCano = larguraTela;
+                marcouPonto = false;
+                variacaoCanos = numAleatorio.nextInt(500) - 250;
+            }
+
+            //caso o cano tenha passado do pássaro, serão marcados pontos
+			if (posicaoHorizontalCano < 200 && marcouPonto == false){
+				pontuacao += 1;
+				marcouPonto = true;
+			}
+
+            //adicionando "gravidade" e queda ao pássaro
+            if (posicaoInicialVertical > 20 || velocidadeQueda < 0) {
+                velocidadeQueda += 0.5;
+                posicaoInicialVertical = posicaoInicialVertical - velocidadeQueda;
+            }
+
+            //adicionando evento de clique e evitando que o pássaro suba mais que o limite da tela
+            if (posicaoInicialVertical < alturaTela) {
+                if (Gdx.input.justTouched()) {
+                    velocidadeQueda -= 12;
+                }
+            } else {
+                posicaoInicialVertical = alturaTela - 50;
+            }
+        }
 
 		//ajeitando a variação de imagens, fazendo o pássaro bater as asas
 		variacao += tempoRenderizacao * 7; //getDeltaTime() pega o intervalo de tempo entre os render
@@ -88,6 +116,7 @@ public class FlappyBird extends ApplicationAdapter {
 		batch.draw(canoTopo, posicaoHorizontalCano,posicaoVerticalCanoTopo + espacoEntreCanos/2 + variacaoCanos);
 		batch.draw(canoBaixo, posicaoHorizontalCano, posicaoVerticalCanoBaixo - espacoEntreCanos/2 + variacaoCanos);
 		batch.draw(passaro[(int) variacao], 200, posicaoInicialVertical);
+		fonte.draw(batch, String.valueOf(pontuacao) ,(larguraTela/2)-25, alturaTela-75);
 
 		batch.end();
 
